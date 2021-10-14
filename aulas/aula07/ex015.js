b209cc07d5052ef1b560b39cdbe69571be485a10
Michael <michael.doug@outlook.com>
2021-10-14 20:41:23 -0300

    Aula 07 - Completamente Finalizada

---
+++ aulas/aula07/ex015.js
@@ -0,0 +1,14 @@
+/* 
+console.log('Olá, tudo bem ?');
+console.log('Olá, tudo bem ?');
+console.log('Olá, tudo bem ?');
+console.log('Olá, tudo bem ?');
+console.log('Olá, tudo bem ?');
+*/
+
+var cont = 1;
+
+while (cont <= 5) {
+    console.log(`Olá, tudo bem ?`);
+    cont += 1;
+}
---
+++ aulas/aula07/ex016.js
@@ -0,0 +1,14 @@
+/*
+console.log('Passo 1');
+console.log('Passo 2');
+console.log('Passo 3');
+console.log('Passo 4');
+console.log('Passo 5');
+*/
+
+var cont = 1;
+
+do {
+    console.log(`Passo ${cont}`);
+    cont += 1;
+} while (cont <= 5);
