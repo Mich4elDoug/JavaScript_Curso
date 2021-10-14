21491fa5bf638838a1ca7a388c185b53aba3f585
Michael <michael.doug@outlook.com>
2021-10-14 20:49:25 -0300

    Desafio da saudação

---
+++ desafios/d002/des_saudacao.js
@@ -0,0 +1,7 @@
+function saudacao(){
+    let name = prompt("Qual é o seu nome?");
+    
+    let age = prompt(`Olá, ${name}! Quantos anos você tem?`);
+    
+    alert(`Acabei de conhecer o/a ${name}, que tem ${age} anos de idade!`);
+}
