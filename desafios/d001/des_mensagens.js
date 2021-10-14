4db3c24d739cdbc11fbe430962c12ba0cb16bdcf
Michael <michael.doug@outlook.com>
2021-10-14 20:49:02 -0300

    Desafio das mensagens

---
+++ desafios/d001/des_mensagens.js
@@ -0,0 +1,11 @@
+function btn_1(){
+    alert('Você clicou no 1° botão!');
+}
+
+function btn_2(){
+    alert('Você clicou no 2° botão!');
+}
+
+function btn_3(){
+    alert('Você clicou no 3° botão!');
+}
