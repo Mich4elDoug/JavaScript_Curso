dffad34a764c7f1d0cccd2d6590eaabc3562921d
Michael <michael.doug@outlook.com>
2021-10-14 20:49:53 -0300

    Desafio dos números

---
+++ desafios/d003/des_numeros.js
@@ -0,0 +1,6 @@
+function numeros(){
+
+    let num = parseInt(prompt("Digite um número inteiro qualquer!"));
+        
+    alert(`Antes de ${num} temos o número ${num-1}. \nDepois de ${num} temos o número ${num+1}. `);
+}
