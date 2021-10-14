f365f08c38ef0f2dec422e39920a3975affb8424
Michael <michael.doug@outlook.com>
2021-10-14 20:41:34 -0300

    Aula 08 - Completamente Finalizada

---
+++ aulas/aula08/ex017.js
@@ -0,0 +1,23 @@
+/*
+
+console.log('Passo 1');
+console.log('Passo 2');
+console.log('Passo 3');
+console.log('Passo 4');
+console.log('Passo 5');
+
+var cont = 1;
+
+while(cont <=5 ){
+    console.log(`Passo ${cont}`);
+}
+
+*/
+
+console.log("Vai começar...");
+
+for (var cont = 1; cont <=5; cont++) {
+    console.log(`Passo ${cont}`);
+}
+
+console.log("FIM...");
