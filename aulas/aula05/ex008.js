c4f3863d4fc5239a9bddba5ae7ec928108bd75cd
Michael <michael.doug@outlook.com>
2021-10-14 20:40:50 -0300

    Aula 05 - Completamente Finalizada

---
+++ aulas/aula05/ex008.js
@@ -0,0 +1,6 @@
+var velocity = 78.5;
+console.log(`A velocidade do seu carro é ${velocity} Km/h`);
+
+if (velocity > 60) {
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!");
+}
---
+++ aulas/aula05/ex009.js
@@ -0,0 +1,9 @@
+var country = "Brasil";
+console.log(`Você vive no ${country}`);
+
+if (country == "Brasil"){
+    console.log('Você é brasileiro!')
+}
+else{
+    console.log('Você é estrangeiro!')
+}
